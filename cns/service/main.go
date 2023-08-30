c8697df16ac6f00336896a8bb1ea77ac780c6f16
Camryn Lee <camrynlee@microsoft.com>
2023-08-30 18:24:17 +0000

    remove additional logger from fsnotify and address comments

--- cns/service/main.go
+++ cns/service/main.go
@@ -80,6 +80,7 @@ const (
 	endpointStoreName                 = "azure-endpoints"
 	endpointStoreLocation             = "/var/run/azure-cns/"
 	watcherPath                       = "/var/run/azure-vnet"
+	watcherDirectory                  = "/deleteIDs"
 	defaultCNINetworkConfigFileName   = "10-azure.conflist"
 	dncApiVersion                     = "?api-version=2018-03-01"
 	poolIPAMRefreshRateInMilliseconds = 1000
@@ -817,7 +818,7 @@ func main() {
 	w := &fsnotify.Watcher{
 		CnsClient: client,
 	}
-	err = fsnotify.WatchFs(w, watcherPath)
+	err = fsnotify.WatchFs(w, watcherPath, watcherDirectory, z)
 	if err != nil {
 		logger.Errorf("Failed to start fsnotify watcher, err:%v.\n", err)
 	}
